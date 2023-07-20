b1586b1498901e147671bb98af86abd98911e08d
ggichuru <ggichuru.dev@gmail.com>
2023-07-21 02:34:32 +0300

    [ wallet ] - wallet balance; pending balance

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math"
+	"math/big"
 	"os"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -38,14 +41,50 @@ func connect_ethclient() ethclient.Client {
 	return *client
 }
 
+func _getBalance(acc_addr common.Address, client ethclient.Client, decimals ...int) (*big.Float, *big.Float) {
+	ctx := context.Background()
+
+	// assign decimals
+	decimal := 18
+	if len(decimals) > 0 {
+		decimal = decimals[0]
+	}
+
+	_balance, err := client.BalanceAt(ctx, acc_addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_pending_bal, err := client.PendingBalanceAt(ctx, acc_addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// convert wei to float
+	balance := new(big.Float)
+	balance.SetString(_balance.String())
+
+	// balance in plaform token
+	bal_eth := new(big.Float).Quo(balance, big.NewFloat(math.Pow10(decimal)))
+
+	balance.SetString(_pending_bal.String())
+
+	// pending balance
+	bal_pending := new(big.Float).Quo(balance, big.NewFloat(math.Pow10(decimal)))
+	return bal_eth, bal_pending
+}
+
 func main() {
 	loadEnv()
 
+	/** GLOBALS*/
 	client := connect_ethclient()
-	fmt.Print(client)
 
-	// convert address to common.address
-	address := common.HexToAddress(os.Getenv("PUBLIC_KEY"))
+	// convert acc_addr to common.Address
+	acc_addr := common.HexToAddress(os.Getenv("PUBLIC_KEY"))
 
-	fmt.Println(address.Hex())
+	/** FN CALLS*/
+	bal_eth, bal_pending := _getBalance(acc_addr, client)
+	fmt.Printf("Wallet Balance: %g MATIC \n", bal_eth)
+	fmt.Printf("Pending Balance: %g MATIC \n", bal_pending)
 }
